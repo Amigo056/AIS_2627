7b82bb1a748415e14611a94d40d032af5f9cc171
CoolfoxgamerXD <coolfoxgamerxd@gmail.com>
2026-09-21 19:25:41 +0100

    cats e cd a funcionst e impressão do cmainho

--- src/Assigment1/bach.c
+++ src/Assigment1/bach.c
@@ -10,6 +10,8 @@
 
 #define MAX_LINE 256
 #define MAX_ARGS 10
+#define PATH_MAX 1024
+char cwd[PATH_MAX];
 
 int cmdParser(char *input, char *processed[], char *del) {
 	char *copy = strdup(input);
@@ -26,10 +28,20 @@ int cmdParser(char *input, char *processed[], char *del) {
 	return idx;
 }
 
+void getCurrentPath() {
+    char newP[PATH_MAX];
+    if (getcwd(newP, sizeof(newP)) == NULL) {
+        perror("getcwd failed");
+    }
+    snprintf(cwd, sizeof(cwd), "\n%s", newP);
+}
+
+
 int main(int argc, char *argv[]){
 	char buffer[MAX_LINE];
-
+	getCurrentPath();
 	while (1) {
+		printf("%s", cwd);
         printf("$ ");
 
         if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
@@ -66,6 +78,14 @@ int main(int argc, char *argv[]){
                     break;
                 }
 
+                if(strcmp(subSubLine[0], "cd") == 0) {
+                    if(chdir(subSubLine[1]) != 0) {
+                        perror("cd failed");
+                    }
+                    getCurrentPath();
+                    continue;
+                }
+
 				int pid = fork();
 
 				if(pid < 0){
@@ -88,9 +108,9 @@ int main(int argc, char *argv[]){
 						return 1;
 					}
 				else if(pid == 0){
-					char *dst = subLine[subSize];
+					char *dst = subLine[subSize - 1];
 
-					subLine[subSize] = NULL;
+					subLine[subSize - 1] = NULL;
 					int fd = open(dst, O_CREAT | O_RDWR | O_TRUNC, 0644);
 
 					if (fd < 0) {
