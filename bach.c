b871f0e28a7dfe05b63bbd4c02e3bc3982d6f2d4
CoolfoxgamerXD <coolfoxgamerxd@gmail.com>
2026-09-21 12:23:50 +0100

    update bach

--- bach.c
+++ bach.c
@@ -8,29 +8,29 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
-int cmdProcessor(char *input, char *processed[], char *del) {
-    char *copy = strdup(input);
-    int idx = 0;
+int cmdParser(char *input, char *processed[], char *del) {
+	char *copy = strdup(input);
+	int idx = 0;
 
-    char *token = strtok(copy, del);
+	char *token = strtok(copy, del);
 
-    while(token != NULL){
-        processed[idx] = strdup(token);
-        idx++;
-        token = strtok(NULL, del);
+	while(token != NULL){
+		processed[idx] = strdup(token);
+		idx++;
+		token = strtok(NULL, del);
 
-        if (processed[idx] == NULL) {
+		if (processed[idx] == NULL) {
             break;
         }
-    }
-    processed[idx] = NULL;
-    return idx;
+	}
+	processed[idx] = NULL;
+	return idx;
 }
 
 int main(int argc, char *argv[]){
-    char buffer[256];
+	char buffer[256];
 
-    while (1) {
+	while (1) {
         printf("$ ");
 
         if (fgets(buffer, sizeof(buffer), stdin) == NULL) {
@@ -41,67 +41,66 @@ int main(int argc, char *argv[]){
 
         char *line[10];
 
-        int size = cmdProcessor(buffer, line, " | ");
+       int size = cmdParser(buffer, line, " | "); // cat hey.txt  hello.txt -> 1
 
-        if(size > 1){
+       if(size > 1){
 
-        }else {
-            char *subLine[10];
+	   }else {
+			char *subLine[10];
 
-            int subSize = cmdProcessor(line[0], subLine, " > ");
+			int subSize = cmdParser(line[0], subLine, " > "); // cat hey.txt hello.txt -> 1
 
-            if(subSize == 1){
-                char *subSubLine[10];
+			if(subSize == 1){
+				char *subSubLine[10];
 
-                int subSubSize = cmdProcessor(subLine[0], subSubLine ," ");
+				int subSubSize = cmdParser(subLine[0], subSubLine ," ");
 
-                int pid = fork();
+				int pid = fork();
 
-                if(pid < 0){
+				if(pid < 0){
+					perror("Fork failed");
+					return 1;
+				}
+				else if(pid == 0){
+					execvp(subSubLine[0], subSubLine);
+					exit(0);
+				}
+				else {
+					int status;
+					pid_t filho_terminado = wait(&status);
+				}
+			}else {
+					int pid = fork();
 
-                    perror("Fork failed");
-                    return 1;
-                }
-                else if(pid ==0){
-                    execvp(subSubLine[0], subSubLine);
-                    exit(0);
-                }
-                else {
-                    int status;
-                    pid_t filho_terminado = wait(&status);
-                }
-            }else {
-                char *dst = subLine[subSize];
-                subLine[subSize] = NULL;
-                int fd = open(dst, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+					if(pid < 0){
+						perror("Fork failed");
+						return 1;
+					}
+				else if(pid == 0){
+					char *dst = subLine[subSize];
 
-                if (fd < 0) {
-                    perror("Erro ao abrir o ficheiro");
-                    exit(1);
-                }
-                dup2(fd, STDOUT_FILENO);
-                close(fd);
-                execvp(subLine[0], subLine);
-            }
+					subLine[subSize] = NULL;
+					int fd = open(dst, O_CREAT | O_RDWR | O_TRUNC, 0644);
 
-        }
+					if (fd < 0) {
+						perror("Erro ao abrir o ficheiro");
+						exit(1);
+					}
+					dup2(fd, 1);
+					close(fd);
+					execvp(subLine[0], subLine);
+					exit(0);
+				}
+				else {
+					int status;
+					pid_t filho_terminado = wait(&status);
+				}
 
-        return 0;
+			}
 
-    }
+		}
+
+    return 0;
+
+	}
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
